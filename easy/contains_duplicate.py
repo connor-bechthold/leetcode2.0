6c1a4de2afcfa34cd5ea775f54008098cf34b28f
Connor Bechthold <connorbechthold@icloud.com>
2022-01-18 21:31:49 -0500

    Update contains_duplicate.py

--- easy/contains_duplicate.py
+++ easy/contains_duplicate.py
@@ -2,6 +2,10 @@
 #O(n) time, O(n) space
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
+        
+        #Keep adding numbers in the list in hash table
+        #If we find a number in the table, return True, as it's a duplicate
+        #Otherwise, return False
         lookup = {}
         for number in nums:
             if number in lookup:
