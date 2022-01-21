cdfe83791400902557209f559849d24a2f4986d0
Connor Bechthold <connorbechthold@icloud.com>
2022-01-20 20:46:30 -0500

    Update linked_list_cycle.py

--- easy/linked_list_cycle.py
+++ easy/linked_list_cycle.py
@@ -8,6 +8,10 @@
 #O(1) space
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
+        
+        #Strategy - fast and slow pointer, move fast pointer two steps for slow pointer's one step
+        #If there's a cycle, the fast pointer will "lap" the slow one, and will point to the same node,
+        #indicating a cycle. If the fast node reaches a "None" node, there is no cycle
         if not head:
             return False
         slow = fast = head
