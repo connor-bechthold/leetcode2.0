1c67683b31937193ef2fc87fa6d1a2283d29db8a
Connor Bechthold <connorbechthold@icloud.com>
2022-01-28 22:38:26 -0500

    Create lowest_common_ancestor_of_a_binary_search_tree.py

---
+++ easy/lowest_common_ancestor_of_a_binary_search_tree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+#My solution, ideal solution
+#O(h) time, O(1) space
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        #Stratgey: Starting from the root of the tree, we simply work down the tree until the p and q nodes split off into different subtrees
+        #The node that this split occurs at will be the LCA
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        elif p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
