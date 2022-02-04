fb83e2d4bb2931085abc753f96cb0a0e0de433e6
Connor Bechthold <connorbechthold@icloud.com>
2022-02-04 00:15:38 -0500

    Update find_the_duplicate_number.py

--- medium/find_the_duplicate_number.py
+++ medium/find_the_duplicate_number.py
@@ -26,3 +26,35 @@ class Solution:
             slow = nums[slow]
             front = nums[front]
         return slow 
+    
+    
+#Other solution, more logical
+#O(nlog(n)) time, O(1) space
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        #We know the numbers in the array range from 1 to len(nums) - 1
+        #Perform binary search in that range
+        #[1,2,3,4] -> We see that in an idea case, the number of entries less
+        #than or equal to any number is that number: Ex for 4, there are 4 numbers <= to 4
+        #However: [1,2,3,3]: If the count is greater than that number, we know we have a duplicate
+        #So, for each binary search iteration, we can iterate through the array and check to see which half of the curr number contains the duplicate:
+        low = 0
+        high = len(nums) - 1
+        
+        #Keep track of the duplicate
+        duplicate = -1
+        
+        while low <= high:
+            mid = (low + high) // 2
+            count = 0
+            for num in nums:
+                if num <= mid:
+                    count += 1
+            
+            if count > mid:
+                duplicate = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+                
+        return duplicate
